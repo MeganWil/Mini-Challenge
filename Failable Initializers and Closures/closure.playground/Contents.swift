82ab843fff85ad04c6953ff17e7d78c0a83ceff5
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-09-02 21:01:55 -0500

    failable init and closure

--- Failable Initializers and Closures/closure.playground/Contents.swift
+++ Failable Initializers and Closures/closure.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var str = "Hello, playground"
+struct MathOperation{
+    var units: String
+    var operation: (Double,Double) -> Double
+    
+    
+    init?(unit: String, operations: @escaping (Double, Double) -> Double) {
+        if (unit == ""){
+            return nil
+        }
+         self.units = unit
+        self.operation = operations
+        }
+}
+
+
+
+
